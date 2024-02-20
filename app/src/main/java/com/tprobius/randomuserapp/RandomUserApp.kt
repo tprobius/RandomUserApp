14041f41cf1b14759d6a26024e173dc26c00ffdc
tprobius <dviktoriyas@gmail.com>
2024-02-20 22:17:40 +0300

    Added RandomUserApp class

---
+++ app/src/main/java/com/tprobius/randomuserapp/RandomUserApp.kt
@@ -0,0 +1,27 @@
+package com.tprobius.randomuserapp
+
+import android.app.Application
+import com.tprobius.randomuserapp.di.apiModule
+import com.tprobius.randomuserapp.di.databaseModule
+import com.tprobius.randomuserapp.di.navigationModule
+import com.tprobius.randomuserapp.di.useCasesModule
+import com.tprobius.randomuserapp.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class RandomUserApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@RandomUserApp)
+            modules(
+                apiModule,
+                databaseModule,
+                useCasesModule,
+                viewModelModule,
+                navigationModule
+            )
+        }
+    }
+}
