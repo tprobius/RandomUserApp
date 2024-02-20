b32da4ee2067ad12787557d693db458751c25ef9
tprobius <dviktoriyas@gmail.com>
2024-02-20 22:17:59 +0300

    Added project dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,9 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -36,12 +39,20 @@ android {
 }
 
 dependencies {
-
+    // Default
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Tests
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Koin
+    implementation("io.insert-koin:koin-android:3.4.3")
+
+    // Cicerone
+    implementation("com.github.terrakok:cicerone:7.1")
 }
