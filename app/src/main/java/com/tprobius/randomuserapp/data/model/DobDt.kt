c797b7a35bf669a25d37f2d15e3acef90596e782
tprobius <dviktoriyas@gmail.com>
2024-02-24 21:40:48 +0300

    Added data caching with Room

--- app/src/main/java/com/tprobius/randomuserapp/data/model/DobDt.kt
+++ app/src/main/java/com/tprobius/randomuserapp/data/model/DobDt.kt
@@ -1,7 +1,6 @@
 package com.tprobius.randomuserapp.data.model
 
 import com.google.gson.annotations.SerializedName
-import com.tprobius.randomuserapp.domain.entities.DobDmn
 
 data class DobDt(
     @SerializedName("date")
