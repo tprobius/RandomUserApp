4b2c25575903b11295c800cae87bb554df215dd1
tprobius <dviktoriyas@gmail.com>
2024-02-25 02:47:31 +0300

    Added unit tests

---
+++ app/src/test/java/com/tprobius/randomuserapp/domain/usecases/GetRandomUsersListUseCaseTest.kt
@@ -0,0 +1,51 @@
+package com.tprobius.randomuserapp.domain.usecases
+
+import com.tprobius.randomuserapp.domain.usecases.data.repository.FakeRandomUserApiRepositoryImpl
+import com.tprobius.randomuserapp.domain.usecases.data.repository.FakeRandomUserDatabaseRepositoryImpl
+import com.tprobius.randomuserapp.domain.entities.RandomUser
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class GetRandomUsersListUseCaseTest {
+
+    private val getRandomUsersListUseCase = GetRandomUsersListUseCase(
+        FakeRandomUserApiRepositoryImpl(),
+        FakeRandomUserDatabaseRepositoryImpl()
+    )
+
+    @Test
+    fun `use case returns expected users list from api repository`() = runTest {
+        val expected = listOf(
+            RandomUser(
+                gender = "Female",
+                first = "Sansa",
+                last = "Stark",
+                city = "Winterfell",
+                state = " the North"
+            )
+        )
+
+        val actual = getRandomUsersListUseCase.invoke(true)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `use case returns expected users list from database repository`() = runTest {
+        val expected = listOf(
+
+            RandomUser(
+                gender = "Male",
+                first = "Jon",
+                last = "Snow",
+                city = "Castle Black",
+                state = "The Wall"
+            )
+        )
+
+        val actual = getRandomUsersListUseCase.invoke(false)
+
+        assertEquals(expected, actual)
+    }
+}
---
+++ app/src/test/java/com/tprobius/randomuserapp/domain/usecases/data/repository/FakeRandomUserApiRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.tprobius.randomuserapp.domain.usecases.data.repository
+
+import com.tprobius.randomuserapp.domain.entities.RandomUser
+import com.tprobius.randomuserapp.domain.repository.RandomUserApiRepository
+
+class FakeRandomUserApiRepositoryImpl : RandomUserApiRepository {
+    override suspend fun getRandomUsersList(): List<RandomUser> {
+        return listOf(
+            RandomUser(
+                gender = "Female",
+                first = "Sansa",
+                last = "Stark",
+                city = "Winterfell",
+                state = " the North"
+            )
+        )
+    }
+}
